b5ea43b6979d0dcd0c097b856775e2e990bdfdea
CH2OLT\wedev <vd.khokh@gmail.com>
2018-10-20 15:47:06 +0300

    NewHorizontsClub

---
+++ Assets/Scripts/ExplosionScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour
+{
+    [SerializeField] private Transform ExplosionPoint;
+    [SerializeField] private GameObject Explosion;
+    bool isShooted = false;
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !isShooted)
+        {
+            GameObject tmp = Instantiate(Explosion, ExplosionPoint.position, Quaternion.Euler(0, 0, 0));
+            tmp.transform.localScale = new Vector3(2, 2, 2);
+            Destroy(tmp, 0.5f);
+
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/TimeBody.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimePoint
+{
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public TimePoint(Vector3 _position, Quaternion _rotation)
+    {
+        position = _position;
+        rotation = _rotation;
+    }
+}
+
+public class TimeBody : MonoBehaviour
+{
+
+    public bool isRewinding = false;
+    private List<TimePoint> recordedPos;
+    private Rigidbody rigid;
+    //---
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        recordedPos = new List<TimePoint>();
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            isRewinding = true;
+        }else if(Input.GetKeyUp(KeyCode.T))
+        {
+            isRewinding = false;
+        }
+
+       
+    }
+
+    private void FixedUpdate()
+    {
+        if(!isRewinding)
+        {
+            Record();
+        }else if(isRewinding)
+        {
+            Rewind();
+        }
+    }
+
+    private void Record()
+    {
+        rigid.isKinematic = false;
+        recordedPos.Insert(0, new TimePoint(transform.position, transform.rotation));
+    }
+
+    private void Rewind()
+    {
+        if(recordedPos.Count - 1 > 1)
+        {
+            rigid.isKinematic = true;
+            transform.position = recordedPos[0].position;
+            transform.rotation = recordedPos[0].rotation;
+            recordedPos.RemoveAt(0);
+        }
+        
+    }
+
+}
